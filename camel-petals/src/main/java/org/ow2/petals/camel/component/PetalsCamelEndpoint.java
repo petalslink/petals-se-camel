341eed6808ba787060c92f60c8871129fe7d32c6
Victor Noël <victor.noel@linagora.com>
2015-04-30 16:39:15 +0200

    Workaround for MEP URI bug in Camel
    It seems they use in-optional-out instead of in-opt-out in the WSDL URI of their MEP.

--- camel-petals/src/main/java/org/ow2/petals/camel/component/PetalsCamelEndpoint.java
+++ camel-petals/src/main/java/org/ow2/petals/camel/component/PetalsCamelEndpoint.java
@@ -61,7 +61,13 @@ public class PetalsCamelEndpoint extends DefaultEndpoint {
         
         this.service = component.getContext().getService(serviceId);
 
-        setExchangePattern(ExchangePattern.fromWsdlUri(service.getMEP().toString()));
+        String mep = service.getMEP().toString();
+        if (mep.contains("in-opt-out")) {
+            // TODO It seems camel has the wrong URI for the pattern InOptionalOut!
+            mep = mep.substring(0, mep.lastIndexOf("/")) + "/in-optional-out";
+        }
+
+        setExchangePattern(ExchangePattern.fromWsdlUri(mep));
     }
 
     /**
