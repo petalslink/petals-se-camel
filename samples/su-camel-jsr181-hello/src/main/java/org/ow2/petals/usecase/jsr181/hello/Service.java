d0228148777a7bef8402ac6e2c0d7481c36dbb46
Victor Noël <victor.noel@linagora.com>
2015-05-13 16:37:28 +0200

    Include the hello jsr181 SU for 4.3 as the samples were not distributed with this version

---
+++ samples/su-camel-jsr181-hello/src/main/java/org/ow2/petals/usecase/jsr181/hello/Service.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2008-2012 EBM WebSourcing, 2012-2015 Linagora
+ *
+ * This program/library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This program/library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program/library; If not, see <http://www.gnu.org/licenses/>
+ * for the GNU Lesser General Public License version 2.1.
+ */ 
+package org.ow2.petals.usecase.jsr181.hello;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+/**
+ * @author Christophe HAMERLING - EBM WebSourcing
+ */
+@WebService(serviceName = "HelloService", portName = "HelloEndpoint", name = "HelloInterface", targetNamespace = "http://petals.ow2.org")
+public class Service {
+
+    public Service() {
+    }
+
+    @WebMethod
+    public String sayHello(String data) {
+        return "You say : " + data;
+    }
+
+}
