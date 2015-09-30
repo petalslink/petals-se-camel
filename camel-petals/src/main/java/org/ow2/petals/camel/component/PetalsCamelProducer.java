0536300924f249c36f4b9c7b2e0f441af9685a51
Victor Noël <victor.noel@linagora.com>
2015-09-30 15:50:52 +0200

    Backport: PETALSSECAMEL-9: Oups, the endpoint name could be empty

--- camel-petals/src/main/java/org/ow2/petals/camel/component/PetalsCamelProducer.java
+++ camel-petals/src/main/java/org/ow2/petals/camel/component/PetalsCamelProducer.java
@@ -139,7 +139,8 @@ public class PetalsCamelProducer extends DefaultAsyncProducer {
                         new ConsumeFlowStepBeginLogData(faAsBC.getFlowInstanceId(), faAsBC.getFlowStepId(),
                                 StringHelper.nonNullValue(exchange.getInterfaceName()),
                                 StringHelper.nonNullValue(exchange.getService()),
-                                StringHelper.nonNullValue(exchange.getEndpointName()),
+                                exchange.getEndpoint() == null ? null
+                                        : StringHelper.nonNullValue(exchange.getEndpoint().getEndpointName()),
                                 StringHelper.nonNullValue(exchange.getOperation())));
             }
 
