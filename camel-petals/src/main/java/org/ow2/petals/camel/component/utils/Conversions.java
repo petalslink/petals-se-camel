505bc01840bba6a309b640e8146816f110013e22
Victor Noël <victor.noel@linagora.com>
2016-02-26 10:06:49 +0100

    Backport: PETALSSECAMEL-16,PETALSSECAMEL-17: header -> property

--- camel-petals/src/main/java/org/ow2/petals/camel/component/utils/Conversions.java
+++ camel-petals/src/main/java/org/ow2/petals/camel/component/utils/Conversions.java
@@ -69,7 +69,7 @@ public class Conversions {
         camelExchange.setProperty(PetalsCamelComponent.EXCHANGE_ORIGINAL_MEP, exchange.getPattern());
 
         for (final Object prop : exchange.getPropertyNames()) {
-            camelExchange.setProperty(PetalsCamelComponent.EXCHANGE_ORIGINAL_HEADER_PREFIX + prop,
+            camelExchange.setProperty(PetalsCamelComponent.EXCHANGE_ORIGINAL_PROPERTY_PREFIX + prop,
                     exchange.getProperty((String) prop));
         }
     }
@@ -124,8 +124,8 @@ public class Conversions {
         Conversions.populateNormalizedMessage(exchange.getInMessage(), camelExchange.getIn());
 
         for (final Entry<String, Object> prop : camelExchange.getProperties().entrySet()) {
-            if (prop.getKey().startsWith(PetalsCamelComponent.EXCHANGE_HEADER_PREFIX)) {
-                exchange.setProperty(prop.getKey().substring(PetalsCamelComponent.EXCHANGE_HEADER_PREFIX.length()),
+            if (prop.getKey().startsWith(PetalsCamelComponent.EXCHANGE_PROPERTY_PREFIX)) {
+                exchange.setProperty(prop.getKey().substring(PetalsCamelComponent.EXCHANGE_PROPERTY_PREFIX.length()),
                         prop.getValue());
             }
         }
